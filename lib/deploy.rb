2f5c051e34bc961b2775bda7962ccd47d79c4a35
Spike Ilacqua <spike@indra.com>
2014-08-19 19:58:44 -0600

    Oops

--- lib/deploy.rb
+++ lib/deploy.rb
@@ -1,15 +1,15 @@
 class Deploy < Sinatra::Base
-  HMAC_DIGEST = OpenSSL::Digest::Digest.new('sha1')
-
-  def verify_signature(body)
-    signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET'], body)
-    Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
-  end
 
   post '/' do
+    body = request.body.read
     halt 403 unless verify_signature(body)
     Dir.chdir('/var/www/html') do
       system('/usr/bin/git pull')
     end
+  end
+
+  def verify_signature(body)
+    signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET'], body)
+    Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
   end
 end
